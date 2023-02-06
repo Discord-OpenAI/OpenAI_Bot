f5f2f73daf3224895ec96cd9ba31e0d3bcef12b9
CoC-Fire <109904491+CoC-Fire@users.noreply.github.com>
2023-02-05 19:04:18 -0800

    Update trainingDataCollector.py

--- trainingDataCollector.py
+++ trainingDataCollector.py
@@ -1,1 +1,15 @@
+"""
+trainingDataCollecter.py
+Made by CoCFire#1111
+
+Training data collection module for OpenAI model fine-tuning
+I'm making it cuz I really need a better way to record the prompts used than the console log files lol
+I'm also not at home, so I can't really work on it cuz I'm making it on mobile data and it's really kinda hard
+Anyway, there's likely gonna be a shit ton of typos until I can actually get a chance to work on it on my pc
+Until then, it's mostly pseudocode
+"""
+# trainingDataCollector.py
+# Made by CoCFire#1111
+
+# Importing modules
 import os, asyncio, discord, openai, requests, json, debug_logger
